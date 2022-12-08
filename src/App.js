4d5175fc16a08c930c44f58ac4a4efc0234eb9ab
luis <luisdonis777@gmail.com>
2022-12-07 20:49:26 -0800

    adding React router Dom

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ function App() {
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Contact Us</Nav.Link>
+            
           </Nav>
         </Container>
       </Navbar>
