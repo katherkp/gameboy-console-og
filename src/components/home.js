326989712de5393ecd765783fbfb15e1cea45e35
luis <luisdonis777@gmail.com>
2022-12-08 19:05:45 -0800

    Adding Background

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Home = () => {
     return (
         <div>
-            <h3>This is Home Page.</h3>
+            <h3>This is The Home Page.</h3>
         </div>
     )
 }
