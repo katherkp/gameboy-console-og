03a161cbc66b69b07f496344c94ae754ea02dc98
katherkp <105694132+katherkp@users.noreply.github.com>
2022-12-08 18:46:42 -0800

    reverted conflicts

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export const Home = () => {
-    return (        
-        <div>
-            <h3>This is Home Page.</h3>
-        </div>
-)
+    return (
+        <Def>
+            <main>
+                <h1>Video Game Review Here</h1>
+                <div>
+                    <img src="/images/kirby-phone.jpg" alt="Kirby playing game on a phone" />
+                <a href="/videogame-review">
+                    <button className="btn-primary">Video Game Review Page</button>
+                </a>
+                </div>
+            </main>
+        </Def>
+    )
 }
+
+module.exports = home
