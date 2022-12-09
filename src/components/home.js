50673dec5ae4924c93dfa280461788be70fd6570
katherkp <105694132+katherkp@users.noreply.github.com>
2022-12-08 18:35:10 -0800

    home issues

--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,9 @@
 import React from 'react'
 
 export const Home = () => {
-    return (
-        <Def>
-            <main>
-                <h1>Video Game Review Here</h1>
-                <div>
-                    <img src="/images/kirby-phone.jpg" alt="Kirby playing game on a phone" />
-                <a href="/videogame-review">
-                    <button className="btn-primary">Video Game Review Page</button>
-                </a>
-                </div>
-            </main>
-        </Def>
-    )
+    return (        
+        <div>
+        <h3>This is Home Page.</h3>
+        </div>
+)
 }
-
-module.exports = home
